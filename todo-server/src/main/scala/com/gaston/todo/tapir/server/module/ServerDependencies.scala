38f33b35c217ca4627fc420afe5be8daa59fdc27
Gastón Schabas <gastonschabas@gmail.com>
2022-08-25 20:48:14 -0300

    fix - jdbc url (#30)

--- todo-server/src/main/scala/com/gaston/todo/tapir/server/module/ServerDependencies.scala
+++ todo-server/src/main/scala/com/gaston/todo/tapir/server/module/ServerDependencies.scala
@@ -55,7 +55,7 @@ trait ServerDependencies {
 
   Flyway.configure
     .dataSource(
-      s"postgres://${databaseProperties.host}:${databaseProperties.port}/${databaseProperties.dbName}",
+      s"jdbc:postgresql://${databaseProperties.host}:${databaseProperties.port}/${databaseProperties.dbName}",
       databaseProperties.user,
       databaseProperties.password
     )
