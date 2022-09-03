bbe16ccdb43768139ea2b902930a9f3cd16b7651
Gastón Schabas <gastonschabas@gmail.com>
2022-09-03 15:25:07 -0300

    32 - install kamon telemetry (#33)
    - installed kamon bundle and apm reporter v2.5.8
    
    - upgraded tapir to v1.0.6
    - upgraded nimbus jose jwt to v9.24.3
    - upgraded macwire to v2.5.8
    - upgraded postgresql driver to v42.5.0
    - upgraded flyway to v9.2.0

--- todo-server/src/main/scala/com/gaston/todo/tapir/server/Main.scala
+++ todo-server/src/main/scala/com/gaston/todo/tapir/server/Main.scala
@@ -1,9 +1,11 @@
 package com.gaston.todo.tapir.server
 
 import com.gaston.todo.tapir.server.runner.ServerImpl
+import kamon.Kamon
 
 object Main extends App {
 
+  Kamon.init()
   val server = new ServerImpl
   server.start()
 
